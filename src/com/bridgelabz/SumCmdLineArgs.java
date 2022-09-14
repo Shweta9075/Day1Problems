bc4731f42c1a2c4bee98c189d13ea8e87e782781
shweta <shwetabirajdar5872gmail.com>
2022-09-14 20:41:18 +0530

    Added:Sum of integer of command line arguments

---
+++ src/com/bridgelabz/SumCmdLineArgs.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class SumCmdLineArgs {
+    public static void main(String[] args) {
+        int sum = 0;
+        int num;
+        System.out.println("Calculates Sum for below Integers");
+        for(int i=0;i<num.length;i++){
+            System.out.println(num[i]);
+            sum = sum + Integer.parseInt(num[i]);
+        }
+        System.out.println("Sum :" + sum);
+    }
+}
